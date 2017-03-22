cd7ba705a849877e6bcbaf2dedf7d46a2a94f66d
dydaev <dydaev@gmail.com>
2017-03-22 12:33:16 +0200

    start git

---
+++ pattModule.js
@@ -0,0 +1,18 @@
+var greeting = (function () {
+    var module = {};
+    var helloInLang = {
+        en: 'Hello world!',
+        es: '¡Hola mundo!',
+        ru: 'Привет, мир!'
+    };
+
+    module.getello = function (lang) {
+        return helloInLang[lang];
+    };
+
+    module.writeHello = function (lang) {
+        document.write(module.getHello(lang))
+    };
+
+    return module;
+}());
